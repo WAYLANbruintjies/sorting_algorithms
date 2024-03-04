5a77bd000ab6177ccf636089c511eee55b7b282c
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-03-04 01:42:06 -0800

    betty corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,7 @@
  * Description: Print array after each swap
  *
  * Return: void
- */ 
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
@@ -18,17 +18,17 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if(array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = temp;
-				
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
 				print_array(array, size);
 			}
 		}
