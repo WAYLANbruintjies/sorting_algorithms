d65da5847e19776327f9d61acf059b72eeb3a5ff
WAYLANbruintjies <waylandyzz632@gmail.com>
2024-03-04 13:50:49 -0800

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,34 @@
 #include "sort.h"
 
 /**
- * selection_sort - function that sorts an array
- *@size:  size of array
- * @array: list with numbers
+ * selection_sort - Sort an array in using Selection sort
+ * @size: size of the array
+ * @array: list of integers
+ * Return: void
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, idx;
-	int temp, min;
+	size_t i, j;
+	int min, temp, flag = 0;
 
 	if (array == NULL)
 		return;
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		min = i;
-		for (idx = i + 1; idx < size; idx++)
+		flag = 0;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[min] > array[idx])
+			if (array[min] > array[j])
 			{
-				min = idx;
+				min = j;
+				flag = flag + 1;
 			}
 		}
 		temp = array[i];
 		array[i] = array[min];
 		array[min] = temp;
-		print_array(array, size);
+		if (flag != 0)
+			print_array(array, size);
 	}
 }
